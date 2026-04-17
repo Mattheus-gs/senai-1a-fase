ff1ff4260096daa1fd10c9ce3fb773b626901372
Mattheus-gs <mattheusgar@gmail.com>
2026-04-16 22:09:01 -0300

    iniciando o JAVASCRIPT

---
+++ 02-JavaScript/1a-aula.js
@@ -0,0 +1,21 @@
+{
+     
+  //let nome, idade
+   // nome = prompt("digite seu nome:")
+    //    idade = prompt("digite sua idade:")
+   // alert("Seu nome é " + nome +"\nSua idade é "+idade)
+    let nota
+    let nota2
+    let soma
+    
+    nota = prompt("Informe um numero aleatorio:")
+    nota = Number(nota)
+    nota2 = prompt("Informe outro numero aleatorio:")
+     nota2 = Number(nota2)
+
+    soma = nota + nota2
+    alert("a soma dos seus numeros é: "+soma)
+
+    
+
+}
