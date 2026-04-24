b55f1391ed53f7f1c0cc480ca387f1bb85c8f934
Mattheus-gs <mattheusgar@gmail.com>
2026-04-23 21:02:47 -0300

    java primeiro testesinho

--- 02-JavaScript/1a-aula.js
+++ 02-JavaScript/1a-aula.js
@@ -4,18 +4,34 @@
    // nome = prompt("digite seu nome:")
     //    idade = prompt("digite sua idade:")
    // alert("Seu nome é " + nome +"\nSua idade é "+idade)
-    let nota
-    let nota2
-    let soma
-    
-    nota = prompt("Informe um numero aleatorio:")
-    nota = Number(nota)
-    nota2 = prompt("Informe outro numero aleatorio:")
-     nota2 = Number(nota2)
+  
+  // ====================================================
 
-    soma = nota + nota2
-    alert("a soma dos seus numeros é: "+soma)
-
+   //   let Produto1
+  //   let Produto2
     
+  //   Produto1 = prompt("Qual o primeiro produto que voce gostaria?")
+  //  // nota = Number(nota)
+  //   Produto2 = prompt("Informe outro numero aleatorio:")
+  //    //nota2 = Number(nota2)
+  //    // alert("Seu pedido tem os seguintes itens:\n "+Produto1 +"\n"+ Produto2)
+
+  // ===============================================  
+
+  let vitorias, empates, pontos
+
+vitorias = prompt("Quantas vitoria o seu time teve?")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Quantos empates o seu time teve?"))
+
+
+pontos  = (vitorias*3) + (empates)
+
+alert("O total de pontos que o seu time fez foi de: "+ pontos)
+
+
+
+
 
 }
