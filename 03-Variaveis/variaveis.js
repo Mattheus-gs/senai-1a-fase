d7f7c34790835c3cea96d140ee62820d0808eb98
Mattheus-gs <mattheusgar@gmail.com>
2026-05-25 22:03:43 -0300

    mais do jogo do dino

--- 03-Variaveis/variaveis.js
+++ 03-Variaveis/variaveis.js
@@ -155,167 +155,119 @@ document.getElementById("RodadasAnt").innerHTML += 'Rodada:' + pontoT + '<br>'
 
 // DINO
 
-let dino1 = 0
-let dino2 = 0
-let dino3 = 0
-let dino4 = 0
-let dino5 = 0
-let dino6 = 0
-let dino7 = 0
-let dino8 = 0
-let dino9 = 0
-let dino10 = 0
+let dinoEscolhido = 0
 let DinoSorteado = 0
 let ValorAPTotal = 0
+let ValorAP = 0
+let nomeDino = ''
+let maximo = 1000
+function digitarSenha(){
+    let senhaD = prompt("Qual a senha para acessar sua conta? ")
+    let senhaO = "dino123"
 
+    if(senhaD == senhaO){
+        alert("O valor total da sua banca no momento é de: R$ " + ValorAPTotal)
+        let limite = confirm("gostaria de criar um limite maximo para premiações")
+        if (limite == true){
+        maximo = Number(prompt("escreva o novo limite: "))
+    }
+    }
+    else{
+        alert("Voce ganhou uma rodada gratis, parabens!!!!!!!!")
+    }
+}
 function Apostar(){
-    const Aposta = Math.ceil(Math.random() * 10);
+    let Aposta = Math.ceil(Math.random() * 10);
     ValorAP = Number(document.getElementById("ValorAposta").value)
-
+    
     ValorAPTotal = ValorAPTotal + ValorAP
 
    
     
     if(Aposta == 1){
-        DinoSorteado = "Dino sorteado: Espinossauro"
-    }else if(Aposta == 2){
-         DinoSorteado = "Dino sorteado: T-rex"
-    }else if(Aposta == 3){
-         DinoSorteado = "Dino sorteado: Bronquiossauro"
-    }else if(Aposta == 4){
-         DinoSorteado = "Dino sorteado: Velociraptor"
-    }else if(Aposta == 5){
-         DinoSorteado = "Dino sorteado: Estegossauro"
-    }else if(Aposta == 6){
-         DinoSorteado = "Dino sorteado: Triceratopo"
-    }else if(Aposta == 7){
-         DinoSorteado = "Dino sorteado: Aquilossauro"
-    }else if(Aposta == 8){
-         DinoSorteado = "Dino sorteado: Parassaurolofo"
-    }else if(Aposta == 9){
-         DinoSorteado = "Dino sorteado: Alossauro"
-    }else if(Aposta == 10){
-         DinoSorteado = "Dino sorteado: Carnotauro"
-    }
-
-
-
-    if(dino1 == Aposta){ 
         document.getElementById("DinoS").innerHTML = "Dino sorteado: Espinossauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-    }else if(dino2 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: T-rex"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino3 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Broquiossauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino4 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Velociraptor"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino5 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Estegossauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino6 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Triceratopo"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino7 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Anquilossauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino8 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Parassaurolofo"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino9 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Alossauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-        }else if(dino10 == Aposta){
-        document.getElementById("DinoS").innerHTML = "Dino sorteado: Carnotauro"
-        ValorAP = ValorAP * 2
-        if(ValorAPTotal < 500){
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ XXX "ERROR 404" Tente novamento, na proxima voce pode levar a sorte grande' 
-            }else{document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)}
-    }else{
-        document.getElementById("DinoS").innerHTML = DinoSorteado
-        ValorAP = 0
-        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$' + ValorAP.toFixed(2)
+    }else if(Aposta == 2){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: T-rex"
+    }else if(Aposta == 3){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Bronquiossauro"
+    }else if(Aposta == 4){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Velociraptor"
+    }else if(Aposta == 5){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Estegossauro"
+    }else if(Aposta == 6){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Triceratopo"
+    }else if(Aposta == 7){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Aquilossauro"
+    }else if(Aposta == 8){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Parassaurolofo"
+    }else if(Aposta == 9){
+         document.getElementById("DinoS").innerHTMLnoSorteado = "Dino sorteado: Alossauro"
+    }else if(Aposta == 10){
+         document.getElementById("DinoS").innerHTML = "Dino sorteado: Carnotauro"
     }
+
+
+
+    if(dinoEscolhido == Aposta){
+        let valorganho = ValorAP * 2
+          console.log(valorganho)
+        let valorlimite = (valorganho/ValorAPTotal) * 100
+            if(valorganho > maximo){
+                document.getElementById("ValorRec").innerHTML = "Valor a receber: R$ XXX ... ERRO 404, tente novamente, na proxima pode ser seu giro da sorte!!!"
+            }
+            else if(valorlimite <= 20){
+                     document.getElementById("50x").innerHTML = '<button  onclick="Multiplicar()">Chance unica: 50x</button>'               
+                ValorAPTotal = ValorAPTotal - valorganho
+                document.getElementById("ValorRec").innerHTML = "Valor a receber: R$ " + valorganho.toFixed(2)
+                document.getElementById("50xtexto").innerText = "Clique no botao para ter a chance de multiplicar seu premio em 50x"
+                
+            }
+            else{document.getElementById("ValorRec").innerHTML = "Valor a receber: R$ XXX ... ERRO 404, tente novamente, na proxima pode ser seu giro da sorte!!!"
+            }
+            
+    }else{
+        ValorAPTotal = ValorAPTotal + ValorAP
+        document.getElementById("ValorRec").innerHTML = 'Valor a receber: R$ 0,00'
+    }
+    console.log(ValorAPTotal)
+    }
+
+
+function DinoE(numeroDino){
+        dinoEscolhido = numeroDino
+        if(dinoEscolhido == 11){
+            dinoEscolhido = Math.ceil(Math.random() * 10)
+        }
+
+        if(dinoEscolhido == 1){
+            nomeDino = 'Seu dino: Espinossauro'
+        }else if(dinoEscolhido == 2){
+            nomeDino = 'Seu dino: T-rex'
+        }else if(dinoEscolhido == 3){
+            nomeDino = 'Seu dino: Bronquissauro'
+        }else if(dinoEscolhido == 4){
+            nomeDino = 'Seu dino: Velociraptor'
+        }else if(dinoEscolhido == 5){
+            nomeDino = 'Seu dino: Estegossauro'
+        }else if(dinoEscolhido == 6){
+            nomeDino = 'Seu dino: Triceratopo'
+        }else if(dinoEscolhido == 7){
+            nomeDino = 'Seu dino: Anquilossauro'
+        }else if(dinoEscolhido == 8){
+            nomeDino = 'Seu dino: Parassaurolofo'
+        }else if(dinoEscolhido == 9){
+            nomeDino = 'Seu dino: Alossauro'
+        }else if(dinoEscolhido == 10){
+            nomeDino = 'Seu dino: Carnotauro'
+        }
+        document.getElementById("SeuDino").innerHTML = nomeDino
+    }
+
     
 
 
-
-
-
-
-
-    console.log(Aposta)
-    console.log(ValorAPTotal)
-}
-
-
-function Espino(){
-    dino1 = 1
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Espinossauro"
-
-}
-function Trex(){
-    dino2 = 2
-    document.getElementById("SeuDino").innerHTML = "Seu dino: T-rex"
-}
-function Broquio(){
-    dino3 = 3
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Bronquiossauro"
-}
-function Veloc(){
-    dino4 = 4
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Velociraptor"
-}
-function Estego(){
-    dino5 = 5
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Estegossauro"
-}
-function Trice(){
-    dino6 = 6
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Triceratopo"
-}
-function Anquilo(){
-    dino7 = 7
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Anquilossauro"
-}
-function Parassau(){
-    dino8 = 8
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Parassaurolofo"
-}
-function Alos(){
-     dino9 = 9
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Alossauro"
-}
-function Carno(){
-    dino10 = 10
-    document.getElementById("SeuDino").innerHTML = "Seu dino: Carnotauro"
-}
+  function Multiplicar(){
+            document.getElementById("ValorRec").innerHTML = "Valor a receber: R$ 0,00 - Infelizmente voce nao teve sorte, tente novamente!!!"
+            document.getElementById("50xtexto").innerText = ""
+                document.getElementById("50x").innerHTML = ''
+    }
